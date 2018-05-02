7175049608101a6fd024899b3040c715134806cd
Ken <k3nndroid@gmail.com>
2018-05-02 01:59:15 -0600

    made changes and now working!

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
 # liri-node-app
+### Overview
+In this assignment, you will make LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
 
-Not  quite ready....working out the bugs!!!
-
-This App uses info from
+## This App uses info from
  * Twitter
  * Spotify
  * OMBD
  
- Commands to Input
+## NPM
+* twitter
+* node spotify api
+* prompt
+* colors
+* request
+
+ ##Commands to Input
  * myTweets
  * spotifySong
  * movieThis
  * do-what-it-says
+
+## Instructions
+1. run the application in node.js
+2. follow prompt commands
+3. repeat steps to check other commands
+#### note: The application will end after each output of results.
--- liri.js
+++ liri.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
-var keys = require("./keys.js");
+var keys = require("./keys");
 var fs = require("fs");
 var Twitter = require("twitter");
-var spotify = require("spotify");
+var Spotify = require("node-spotify-api");
 var request = require("request");
 var prompt = require("prompt");
 var colors = require("colors/safe");
+const util = require("util");
 
 //----------For user inputs----------------------------
 var userInput = "";
@@ -18,33 +19,55 @@ var movieThis = "movieThis";
 var doWhatItSays = "do-what-it-says";
 
 //---------setting up the prompts-----------------------
-prompt.message = colors.grey.bold("Type in one of the commands: myTweets, spotifySong, movieThis, do-what-it-says");
+
+prompt.message = colors.red("myTweets, spotifySong, movieThis, do-what-it-says");
 prompt.delimiter = colors.green("\n");
 
 prompt.start();
 prompt.get({
     properties: {
         userInput: {
-            description: colors.blue("Type a command")
+            description: colors.blue("Type in one of the commands above")
         }
     }
-}, function(err, result) {
+}, function(err, result) {              // Function will run base on user's commands
         userInput = result.userInput;
-        if (userInput === myTweets) {
+        if (userInput === myTweets) {       // myTwitter
             // console.log("User Input: "+userInput)
             myTwitter();
-        } else if (userInput === spotifySong) {
-            prompt.get({properties: {userSelection: {description: colors.blue("Search for a song?")}}
+            // console.log("Twt prompt User Selection: " + userSelection + " | " + userInput);
+        } 
+        else if (userInput === spotifySong) {       // Spotify
+            prompt.get({
+                properties: {
+                    userSelection: {
+                        description: colors.blue("Search for a song?")
+                    }
+                }
             }, function (err, result) {
                 if (result.userSelection === "") {
                     userSelection = "Poker Face";
-                } else {
-                    userSelection = result.userInput;
+                    // console.log("prompt result: " + result);
+                    // console.log("prompt User_Selection: " + userSelection);
+                } 
+                else {
+                    userSelection = result.userSelection;
+                    // console.log("prompt result: " + result);
+                    // console.log(util.inspect(result, {showHidden: false, depth: null}))
+                    // console.log("prompt User Selection: " + userSelection);
+                    // console.log("prompt User Selection: " + result.userSelection);
                 }
-                mySpotify();
+                mySpotify(userSelection);
+                // console.log("Spotify prpt User Selection: " + userSelection);
             });
-        } else if (userInput === movieThis) {
-            prompt.get({properties: {userSelection: {description: colors.blue("Search a movie title?")}}
+        } 
+        else if (userInput === movieThis) {     // IMBD Movies
+            prompt.get({
+                properties: {
+                    userSelection: {
+                        description: colors.blue("Search a movie title?")
+                    }
+                }
             }, function(err, result) {
                 if (result.userSelection === "") {
                     userSelection = "Terminator";
@@ -53,27 +76,30 @@ prompt.get({
                 userSelection = result.userSelection;
                 }
                 myMovies(userSelection);
+                // console.log("Movie prompt User Selection: " + userSelection);
             });
-        } else if (userInput === doWhatItSays) {
+        } 
+        else if (userInput === doWhatItSays) {      // This will read a random text in the .txt file
             doWhadeedee();
         };
     }
 );
+
 // ---------Twitter Function-------------------
 function myTwitter() {
     var client = new Twitter(keys.twitter);
     // console.log("Client: " + client)
 
-    var params = {user_name: "scroopops", count: 20};
+    var params = {screen_name: "scroopops"};
     // console.log("Params: " + params)
     
     client.get("statuses/user_timeline", params, function(error, tweets, response) {
         if(!error) {
             for (var i = 0; i < tweets.length; i++) {
                     var tweetInfo = 
-                        colors.green("created: ") + tweets[i].created_at + "\n" +
-                        colors.green("Tweets: ") + tweets[i].text + "\n" +
-                        colors.green("---------------------------------------\n");
+                        colors.blue("created: ") + tweets[i].created_at + "\n" +
+                        colors.blue("Tweets: ") + tweets[i].text + "\n" +
+                        colors.blue("---------------------------------------------\n");
                     
                     console.log(tweetInfo);
             };
@@ -85,26 +111,44 @@ function myTwitter() {
     });
 };
 
+// // ==================== spotify debugging area ===========================
+// // var songName = process.argv[2];
+// // console.log(process.argv)
+
+// // var params = songName;
+// // var spotify = new Spotify(keys.spotify)
+
+// // console.log("params: "+params)
+// //     spotify.search({type:'track', query:params }, function(err, data) {
+// //         console.log(data)
+// // ==================================================================
+
 // -------------Spotify Function-----------------------
 function mySpotify(userSelection) {
+    var spotify = new Spotify(keys.spotify)
     spotify.search({type: "track", query: userSelection}, function(err, data) {
         if (err) {
             console.log("ERROR: " + err);
             return;
         }
+        // console.log("User Selection: " + userSelection);
+        // console.log("DATA: " + data);
+        // console.log(util.inspect(data, {showHidden: false, depth: null}))
+        
         var song = data.tracks.items;
         for (var i = 0; i < song.length; i++) {
             var songInfo = 
-                colors.blue("Artist: ") + song[i].artists[0].name + "\n" +
-                colors.blue("Song Name: ") + song[i].name + "\n" +
-                colors.blue("A preview link of the song from Spotify: ") + song[i].preview_url + "\n" +
-                colors.blue("The album that the song is from: ") + song[i].album.name + "\n" +
-                colors.blue("-----------------------------------------------------\n");
+                colors.green("Artist: ") + song[i].artists[0].name + "\n" +
+                colors.green("Song Name: ") + song[i].name + "\n" +
+                colors.green("A preview link of the song from Spotify: ") + song[i].preview_url + "\n" +
+                colors.green("The album that the song is from: ") + song[i].album.name + "\n" +
+                colors.green("--------------------------------------------------------------------------\n");
 
             console.log(songInfo);
         }
     });
 };
+    // })
 
 // ----------------OMBD function ------------------
 function myMovies(title) {
@@ -139,4 +183,3 @@ function doWhadeedee() {
         mySpotify(text[1]);
     })
 }
-
